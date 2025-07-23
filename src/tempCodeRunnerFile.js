e687d21982f81ab94dfa30fbf3bb219112dcc128
manish0502525 <manish7773148@gmail.com>
2025-07-23 13:28:41 +0530

    first commit

---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,91 @@
+return (
+    <React.Fragment>
+      <AppBar position="static" className={classes.appbar}>
+        <Toolbar>
+          <Typography className={classes.title} variant="h6" noWrap>
+            Plant Disease Detection Web by Manish
+          </Typography>
+          <div className={classes.grow} />
+          <Avatar src={cblogo}></Avatar>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth={false} className={classes.mainContainer} disableGutters={true}>
+        <Grid
+          className={classes.gridContainer}
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          spacing={2}
+        >
+          <Grid item xs={12}>
+            <Card className={`${classes.imageCard} ${!image ? classes.imageCardEmpty : ''}`}>
+              {image && (
+                <CardActionArea>
+                  <CardMedia
+                    className={classes.media}
+                    image={preview}
+                    component="image"
+                    title="Contemplative Reptile"
+                  />
+                </CardActionArea>
+              )}
+              {!image && (
+                <CardContent className={classes.content}>
+                  <DropzoneArea
+                    acceptedFiles={['image/*']}
+                    dropzoneText={"Drag and drop an image of a plant leaf to process"}
+                    onChange={onSelectFile}
+                  />
+                </CardContent>
+              )}
+              {data && (
+                <CardContent className={classes.detail}>
+                  <TableContainer component={Paper} className={classes.tableContainer}>
+                    <Table className={classes.table} size="small" aria-label="simple table">
+                      <TableHead className={classes.tableHead}>
+                        <TableRow className={classes.tableRow}>
+                          <TableCell className={classes.tableCell1}>Label:</TableCell>
+                          <TableCell align="right" className={classes.tableCell1}>Confidence:</TableCell>
+                        </TableRow>
+                      </TableHead>
+                      <TableBody className={classes.tableBody}>
+                        <TableRow className={classes.tableRow}>
+                          <TableCell component="th" scope="row" className={classes.tableCell}>
+                            {data.class}
+                          </TableCell>
+                          <TableCell align="right" className={classes.tableCell}>{confidence}%</TableCell>
+                        </TableRow>
+                      </TableBody>
+                    </Table>
+                  </TableContainer>
+                </CardContent>
+              )}
+              {isLoading && (
+                <CardContent className={classes.detail}>
+                  <CircularProgress color="secondary" className={classes.loader} />
+                  <Typography className={classes.title} variant="h6" noWrap>
+                    Processing
+                  </Typography>
+                </CardContent>
+              )}
+            </Card>
+          </Grid>
+          {data && (
+            <Grid item className={classes.buttonGrid}>
+              <ColorButton
+                variant="contained"
+                className={classes.clearButton}
+                color="primary"
+                component="span"
+                size="large"
+                onClick={clearData}
+                startIcon={<Clear fontSize="large" />}
+              >
+                Clear
+              </ColorButton>
+            </Grid>
+          )}
+        </Grid>
+      </Container>
+    </React.Fragment>
